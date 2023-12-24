7eb6c7f124b81fa3c16b91b003fc61ebb183d2b7
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2023-12-24 19:07:03 +0530

    upload code

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -54,20 +54,63 @@ const register = async (req, res) => {
 
 
 //======================================================================================================
-const login = (req,res) => {
+const login = async (req,res) => {
+    try {
+        const { email, password} = req.body;
 
+    if(!email || !password){
+        return next(new AppError('All fields are required',400))
+    }
+
+    const user = await User.findone({
+        email
+    }).select('+password')
+
+    if(!user || !user.comparePassword(password)){
+        return next (new AppError('Email or password does not match',400))
+    }
+     
+    const token = await user.generateJWTToken();
+    user.password = undefined ;
+
+    res.cookie('token',token, cookieOptions)
+
+    res.status(200).json({
+        success: true,
+        message: 'User loggedin successfully',
+        user,
+    })
+    } catch (error) {
+        return next (new AppError(error.message,500))
+    }
+    
 
 }
 
 const logout = (req,res) => {
+     res.cookie('token', null, {
+        secure: true,
+        maxAge: 0,
+        httpOnly: true
+     });
 
+     res.status(200).json({
+        success: true,
+        messsage: 'User logged out  successfully'
+     })
 
 }
 
 
-const getProfile = (req, res ) => {
-
-
+const getProfile = async (req, res ) => {
+     try {
+        const userId = req.body.id;
+        const user = await User.findById(userId)
+        
+     } catch (error) {
+        
+     }
+     
 }
 
 
