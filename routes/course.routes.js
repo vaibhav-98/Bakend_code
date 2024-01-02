f2b960636384f2d7644ac0823395dc884718b51e
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2024-01-02 18:33:48 +0530

    upload  course route

--- routes/course.routes.js
+++ routes/course.routes.js
@@ -1,10 +1,17 @@
 import { Router } from "express"
-import { getAllCourses, getLecturesByCourseId } from "../controllers/course.controllers.js";
-
+import { createCourse, getAllCourses, getLecturesByCourseId, removeCourse, updateCourse } from "../controllers/course.controllers.js";
+import { isLoggedIn } from '../middlewares/auth.middleware.js'
+import upload from '../middlewares/multer.middleware.js'
 const router = Router()
 
-router.get('/', getAllCourses);
+router.route('/')
+      .get(getAllCourses)
+      .post(upload.single('thumbnail'),createCourse)
+      
 
-router.get('/', getLecturesByCourseId)
+router.route('/:id')
+       .get( isLoggedIn,getLecturesByCourseId)
+       .put(updateCourse)
+       .delete(removeCourse)
 
 export default router
